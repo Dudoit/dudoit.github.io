ced33425a38623fd86410908ece242a316c501f9
都峥 <1551362082@qq.com>
2023-08-30 15:45:40 +0800

    deploy

---
+++ assets/chunks/cdnImg.vue_vue_type_style_index_0_lang.9f1de3f2.js
@@ -0,0 +1,1 @@
+import{d as n,o as s,c}from"./framework.a3f41f98.js";const o=["alt"],m=n({__name:"cdnImg",props:["src","alt"],setup(e){const t="https://cdn.jsdelivr.net/gh/Dudoit/resources@blog0.0.4/blog/images/"+e.src+".png";return(l,a)=>(s(),c("img",{class:"cdn-img",src:t,alt:e.alt},null,8,o))}});export{m as _};
